a97f107590a3f36e84edb0054d236db74c9de640
mia <85103168+mia-738@users.noreply.github.com>
2024-06-27 11:43:50 +0900

    Update user_role_change_app.php

--- html/auth/user_role_change_app.php
+++ html/auth/user_role_change_app.php
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         ),
     );
     $context  = stream_context_create($options);
-    $result = file_get_contents('http://127.0.0.1:5000/change_role', false, $context);
+    $result = file_get_contents('http://****/change_role', false, $context);
     
     if ($result === FALSE) { 
         echo 'Error connecting to server.';
