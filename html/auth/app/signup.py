6668fd90aac5c0cb67bb24b130d1445626c80f39
mia <85103168+mia-738@users.noreply.github.com>
2024-06-29 00:12:04 +0900

    Update signup.py

--- html/auth/app/signup.py
+++ html/auth/app/signup.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../../..'))
 
 from app import app, db, mail, User
 
-signup_bp = Blueprint('signup', __name__)
+signup_bp = Blueprint('signup_request', __name__)
 s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 
 @signup_bp.route('/signup_request', methods=['POST'])
