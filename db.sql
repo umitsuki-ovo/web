29093742e7b4a3de9aa428bc3f6c8527e2c2a51d
mia <85103168+mia-738@users.noreply.github.com>
2024-07-20 00:17:44 +0900

    Delete db.sql

--- db.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    email TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL,
-    confirmed BOOLEAN DEFAULT FALSE,
-    role TEXT DEFAULT 'user'
-);
