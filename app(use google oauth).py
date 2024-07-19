757da9bef4b02e033caba77b511cc8c1323ceafd
mia <85103168+mia-738@users.noreply.github.com>
2024-07-19 23:50:41 +0900

    Update app(use google oauth).py

--- app(use google oauth).py
+++ app(use google oauth).py
@@ -3,10 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 import secrets
 import sys
 import os
-import json
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import Flow
-from googleapiclient.discovery import build
 
 app = Flask(__name__, static_folder='html', template_folder='html')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
@@ -35,23 +31,9 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'html/auth/app'))
 
 from signup import signup_bp
 from login import login_bp
-from pass_reset import pass_reset_bp
 
 app.register_blueprint(signup_bp)
 app.register_blueprint(login_bp)
-app.register_blueprint(pass_reset_bp)
-
-def send_email(to, subject, body):
-    credentials = Credentials.from_authorized_user_file('****.json') #Your token json file
-    service = build('gmail', 'v1', credentials=credentials)
-    message = {
-        'raw': base64.urlsafe_b64encode(
-            f"To: {to}\r\nSubject: {subject}\r\n\r\n{body}".encode("utf-8")
-        ).decode("utf-8")
-    }
-    send_message = service.users().messages().send(userId="me", body=message).execute()
-    print(f'Message Id: {send_message["id"]}')
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
-
