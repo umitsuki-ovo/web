3df37ded61e2c3d8634bcdb054f54ba46c7c033c
umitsuki <85103168+umitsuki-ovo@users.noreply.github.com>
2024-07-29 19:52:26 +0900

    Update user_index.php

--- html/user/user_index.php
+++ html/user/user_index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Main Page</title>
-    <link rel="stylesheet" href="root.css">
-    <link rel="stylesheet" href="./user/styles.css">
+    <link rel="stylesheet" href="../html/root.css">
+    <link rel="stylesheet" href="../html/user/styles.css">
     <script>
         function toggleUserMenu() {
             const userMenu = document.getElementById('user-menu');
