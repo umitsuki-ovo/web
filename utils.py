2c73d1c794ede072a18d1e5efcfdb81a8578e7d5
umitsuki <85103168+umitsuki-ovo@users.noreply.github.com>
2024-07-29 20:05:58 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -19,6 +19,4 @@ def send_email(to, subject, body):
     raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode('utf-8')
     raw_message = {'raw': raw_message}
     send_message = service.users().messages().send(userId='me', body=raw_message).execute()
-
-    # Debug code
-    # print(f'Message Id: {send_message["id"]}')
+    print(f'Message Id: {send_message["id"]}')
