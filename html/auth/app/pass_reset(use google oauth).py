323fdc4715e663c580ad4258dd3a23b09df9d643
umitsuki <85103168+umitsuki-ovo@users.noreply.github.com>
2024-07-29 20:59:25 +0900

    Update pass_reset(use google oauth).py

--- html/auth/app/pass_reset(use google oauth).py
+++ html/auth/app/pass_reset(use google oauth).py
@@ -7,46 +7,53 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../..'))
 
-from app import app, db, mail, User
+from app import app, db, User
 from utils import send_email
 
 pass_reset_bp = Blueprint('pass_reset', __name__)
 s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 
+# Reset app
 @pass_reset_bp.route('/reset_password_request', methods=['POST'])
 def reset_password_request():
     email = request.form.get('email')
     user = User.query.filter_by(email=email).first()
+
     if user:
         token = s.dumps(email, salt='password-reset')
-        reset_url = url_for('pass_reset_request.reset_password', token=token, _external=True)
-        # here can change message
+        reset_url = url_for('pass_reset.reset_password', token=token, _external=True)
+        # Here can change message
         html = f'''
-        <p>Hi {user.username},</p>
+        <h1>Hi {user.username},</h1>
         <p>To reset your password, click the link below:</p>
         <p><a href="{reset_url}">Reset Password</a></p>
         <p>If you did not request a password reset, please ignore this email.</p>
         '''
         send_email(email, 'Confirm your account', html)
-    return jsonify({'message': 'If your email is registered, you will receive a password reset link.'}), 200
+    json = {'message': 'Cheack your mail.', 'p': 'If your email is registered, you will receive a password reset link.'}
+    return render_template('./redirect_page/correct.html', json=json)
 
 @pass_reset_bp.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
     try:
         email = s.loads(token, salt='password-reset', max_age=600)
     except:
-        return jsonify({'message': 'The password reset link is invalid or has expired.'}), 400
+        json = {'message': 'Link error', 'p': 'The password reset link is invalid or has expired.'}
+        return render_template('./redirect_page/error.html', json=json)
     if request.method == 'POST':
         password = request.form.get('password')
         password_confirm = request.form.get('password_confirm')
         if password != password_confirm:
-            return jsonify({'message': 'Passwords do not match.'}), 400
+            json = {'message': 'Passwords do not match.', 'p': 'Re-enter your passwords.'}
+            return render_template('./redirect_page/error.html', json=json)
         user = User.query.filter_by(email=email).first_or_404()
         user.password_hash = generate_password_hash(password)
         db.session.commit()
-        return jsonify({'message': 'Your password has been reset.'}), 200
+        json = {'message': 'Your password has been reset.', 'p': 'You can login new passwords.'}
+        return render_template('./redirect_page/correct.html', json=json)
     return render_template('./auth/reset_password_request.php')
 
+# Reset page
 @pass_reset_bp.route('/pass_reset')
 def pass_reset():
     return render_template('./auth/reset_password_request.php')
