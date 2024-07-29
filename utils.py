9db28ffb5b40ee6316e8f7dc3c462c4f8cdd27c6
umitsuki <85103168+umitsuki-ovo@users.noreply.github.com>
2024-07-29 19:50:06 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,15 +2,21 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 import base64
 import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 def send_email(to, subject, body):
-    credentials = Credentials.from_authorized_user_file('token.json')# Your token file
+    # Load google oauth2 token
+    credentials = Credentials.from_authorized_user_file(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'token.json')) # Your token file
     service = build('gmail', 'v1', credentials=credentials)
-    message = {
-        'raw': base64.urlsafe_b64encode(
-            f"To: {to}\r\nSubject: {subject}\r\n\r\n{body}".encode("utf-8")
-        ).decode("utf-8")
-    }
-    send_message = service.users().messages().send(userId="me", body=message).execute()
-    print(f'Message Id: {send_message["id"]}')
+    message = MIMEMultipart('alternative')
 
+    # Create html mail
+    message['To'] = to
+    message['Subject'] = subject
+    mime_text = MIMEText(body, 'html')
+    message.attach(mime_text)
+    raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode('utf-8')
+    raw_message = {'raw': raw_message}
+    send_message = service.users().messages().send(userId='me', body=raw_message).execute()
+    print(f'Message Id: {send_message["id"]}')
