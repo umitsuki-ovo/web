7d4418b1f396d822a13f59bef85e9295902c17ad
umitsuki <85103168+umitsuki-ovo@users.noreply.github.com>
2024-07-30 11:36:27 +0900

    Update destroy_app.py

--- html/auth/destroy_app.py
+++ html/auth/destroy_app.py
@@ -1,1 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
+    $data = array(
+        'username' => $username, 
+        'password' => $password
+    );
+
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/json\r\n",
+            'method'  => 'POST',
+            'content' => json_encode($data),
+        ),
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents('http://****/delete_user_request', false, $context);
+    
+    if ($result === FALSE) {
+        echo "Error deleting user.";
+    }
+}
+?>
