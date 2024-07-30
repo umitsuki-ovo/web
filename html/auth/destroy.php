212edeee6986f939b7801e1f4fcc717ee62c70b5
umitsuki <85103168+umitsuki-ovo@users.noreply.github.com>
2024-07-30 23:10:20 +0900

    Update destroy.php

--- html/auth/destroy.php
+++ html/auth/destroy.php
@@ -1,18 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Delete User</title>
-</head>
-<body>
-    <h2>Delete User</h2>
+{% extends "template.html" %}
+
+
+{% block title %}
+Delete user
+{% endblock  %}
+
+
+{% block files %}
+
+{% endblock  %}
+    
+
+{% block content %}
+<h2>Delete User</h2>
     <form action="{{url_for('delete_user_request.delete_user_request')}}" method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
-        <br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
-        <br>
+        <label for="username">Username:</label><br>
+        <input type="text" id="username" name="username" required><br><br>
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
         <input type="submit" value="Delete User">
     </form>
-</body>
-</html>
+{% endblock  %}
