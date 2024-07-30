d4d86dcd71e49eef408559e02dbea9d77cf61eac
umitsuki <85103168+umitsuki-ovo@users.noreply.github.com>
2024-07-30 12:12:38 +0900

    Update app(use flaskmail).py

--- app(use flaskmail).py
+++ app(use flaskmail).py
@@ -14,6 +14,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_API')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 mail = Mail(app)
 
